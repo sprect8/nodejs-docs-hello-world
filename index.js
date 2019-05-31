e6805b7bdbe20b4732447f9e2551f58cb21b750c
Victa Phu <sprect8@gmail.com>
2019-05-31 14:16:33 +0800

    Validate

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end("<img src='https://upload.wikimedia.org/wikipedia/en/f/ff/SuccessKid.jpg' style='height:100%'>");
 });
 
 const port = process.env.PORT || 1337;
